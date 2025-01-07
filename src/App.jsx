51d21e614d061d1805ad3558b3cf4edb9155a3a4
ChauhanShivang <chauhanshivang2003@gmail.com>
2025-01-07 21:48:37 +0530

    Updated source files and fixed build issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
 
